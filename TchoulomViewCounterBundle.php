be6f62540f235e77e26f431baa1fb50fa28ec930
tchoulom <tchoulomernest@yahoo.fr>
2019-06-17 18:46:13 +0200

    Adding Statistics

--- TchoulomViewCounterBundle.php
+++ TchoulomViewCounterBundle.php
@@ -14,14 +14,33 @@
 
 namespace Tchoulom\ViewCounterBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tchoulom\ViewCounterBundle\DependencyInjection\Compiler\ViewcounterPass;
 
 class TchoulomViewCounterBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ViewcounterPass());
+    }
+
     /**
      * The supported view interval.
      *
      * @var array
      */
     const SUPPORTED_INTERVAL = ['increment_each_view', 'unique_view', 'daily_view', 'hourly_view', 'weekly_view', 'monthly_view'];
+
+    /**
+     * The supported statistics keys.
+     *
+     * @var array
+     */
+    const SUPPORTED_STATS = ['use_stats', 'stats_extension'];
 }
